1e1f5ff3c8c584e6f5c4030343538b18305fcd3a
KyleMalabuyoc <kam688@scarletmail.rutgers.edu>
2020-11-27 12:06:03 -0500

    added angular material modules

--- CoffeeBreak/src/app/app.module.ts
+++ CoffeeBreak/src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainDashboardComponent } from './main-dashboard/main-dashboard.component';
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainDashboardComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
